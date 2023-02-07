72284ec76951183dac9362fc5c7d029cb3ca0785
chominseok <qorxjaos45@naver.com>
2023-02-07 23:51:38 +0900

    ㅂ

--- study/src/main/java/com/example/demo/Study.java
+++ study/src/main/java/com/example/demo/Study.java
@@ -9,6 +9,7 @@ import java.io.OutputStreamWriter;
 public class Study extends Car{
 	public static void main (String[] args) {
 		// 2222주석 33333333테스트
+		// 주테
 		//Sonata test = new Sonata();
 		Car car = new Minseok();
 		car.testFn();
